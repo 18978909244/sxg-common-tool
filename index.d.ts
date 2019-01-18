1472dbbd0f7dde63a52916a28fa1ef317249be0c
suxg <suxg@zhujia100.com>
2019-01-18 10:14:41 +0800

    new

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,4 @@
-interface fn {
-    (arr : array) : number
-}
-
-interface main {
-    getAvg:fn = getAvg
-}
-
-module.exports = main
+// Type definitions for ./index.js
+// Project: [LIBRARY_URL_HERE] 
+// Definitions by: [YOUR_NAME_HERE] <[YOUR_URL_HERE]> 
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
