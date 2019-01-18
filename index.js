cb0304c15825bd73b90299b502291e32f23b18cf
suxg <suxg@zhujia100.com>
2019-01-18 10:07:11 +0800

    update

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ module.exports =  {
     getAvg:(arr)=>{
         let fn = (prev,next,index,arr)=>(index==arr.length-1)?(prev+next)/arr.length:prev+next
         return arr.reduce(fn,0)
+    },
+    sum:(...arg)=>{
+        let total=0
+        for(let i =0;i<arg.length;i++){
+            total+=arg[i]
+        }
+        return total
     }
 }
