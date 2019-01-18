3e49fb93b71d2c818be8985f60c7afcfff17a588
suxg <suxg@zhujia100.com>
2019-01-18 09:56:55 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -5,3 +5,5 @@ interface getAvg {
 interface getAvg {
     getAvg:getAvg = getAvg
 }
+
+module.exports = getAvg
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default {
+module.exports =  {
     getAvg:(arr)=>{
         let fn = (prev,next,index,arr)=>(index==arr.length-1)?(prev+next)/arr.length:prev+next
         return arr.reduce(fn,0)
