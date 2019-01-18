bc296a5946b6eb1e03b545578788634e413233c6
suxg <suxg@zhujia100.com>
2019-01-18 10:17:36 +0800

    new

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,1 @@
-// Type definitions for ./index.js
-// Project: [LIBRARY_URL_HERE] 
-// Definitions by: [YOUR_NAME_HERE] <[YOUR_URL_HERE]> 
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
+declare var getAvg:function
